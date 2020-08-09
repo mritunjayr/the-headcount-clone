9deaf5dc77922533d7a5977fcc670342bfe66484
mritunjay <mritunjaykumar694@gmail.com>
2020-08-09 19:49:03 +0530

    responsive bug fix

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 
 var lastId, topMenu, topMenuHeight, menuItems;
 var scrollItems, fromTop;
+var cardAnimated;
+//Cache reference to window and animation items
+var $animation_elements = $('.card:not(.w3-animate-left)');
+var $window = $(window);
 
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+        //check to see if this current container is within viewport
+        if ((element_bottom_position <= window_bottom_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('w3-animate-left');
+
+        }
+    });
+}
+topMenu = $("#mainNav"), topMenuHeight = topMenu.outerHeight() + 1
+menuItems = topMenu.find("a");
+scrollItems = menuItems.map(function () {
+    var item = $($(this).attr("href"));
+    if (item.length) { return item; }
+});
 $(function () {
+    $animation_elements = $('.card');
 
     $('#cancel-btn').click(() => {
         $('#mobilemainNav').css("display", "none");
@@ -11,14 +40,11 @@ $(function () {
         $('#mobilemainNav').css("display", "block");
     });
 
-    topMenu = $("#mainNav"), topMenuHeight = topMenu.outerHeight() + 1,
-        // All list items
-        menuItems = topMenu.find("a");
 
-    scrollItems = menuItems.map(function () {
-        var item = $($(this).attr("href"));
-        if (item.length) { return item; }
-    });
+    // All list items
+
+
+
     $(window).scroll(function () {
         // Get container scroll position
         fromTop = $(this).scrollTop() + topMenuHeight;
@@ -48,14 +74,30 @@ $(function () {
                 menuItems.filter("[href='#" + main + "']").parent().addClass("nav-active");
             }
         }
-        $('.card').each(index => {
-            if (($('.card').eq(index).offset().top) < bottom_of_screen)
-                $('.card').eq(index).addClass('w3-animate-left')
-        });
+        // if (cardAnimated == false && ($('.card').eq(0).offset().top < bottom_of_screen)) {
+        //     cardAnimated = true;
+        //     cardAnimation();
+        // }
     });
+    $('#contact-form').submit((event) => {
+        event.preventDefault();
+        $('#contact-form input').each(ele =>
+            $('#contact-form input').eq(ele).val('')
+        );
+        $('#contact-form textarea').val('');
+        $('#form-btn').attr('disabled', 'true');
+        $('#contact-form-message').css('visibility', 'visible');
+    });
+
+    $window.on('scroll resize', check_if_in_view);
 
 });
 
+function cardAnimation() {
+    $('.card:not(.w3-animate-left)').each(index => {
+        $('.card').eq(index).addClass('w3-animate-left')
+    });
+}
 
 function hideMobileMenu() {
     $('#mobilemainNav').css("display", "none");
@@ -70,3 +112,4 @@ $(function () {
         $(event.target).addClass('nav-active');
     })
 });
+
